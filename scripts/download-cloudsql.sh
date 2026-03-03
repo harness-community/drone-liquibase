c6c87071962fde544f094d1b8edcc7695e20bc83
Nirmal Sarswat <nirmal.sarswat@harness.io>
2026-03-03 15:11:37 +0530

    update version format

--- scripts/download-cloudsql.sh
+++ scripts/download-cloudsql.sh
@@ -4,7 +4,13 @@ set -eux
 # Download CloudSQL Socket Factory shaded JAR from Google Artifact Registry
 # This JAR contains all Cloud SQL dependencies with properly merged SPI files
 
-CLOUDSQL_VERSION="1.28.2"
+# Version format: <internal_version>-<cloudsql_base_version>
+# Example: 1.0.0-1.28.1 where 1.0.0 is internal version, 1.28.1 is Cloud SQL Socket Factory version
+
+INTERNAL_VERSION="1.0.0"
+CLOUDSQL_BASE_VERSION="1.28.1"
+CLOUDSQL_VERSION="${INTERNAL_VERSION}-${CLOUDSQL_BASE_VERSION}"
+
 GAR_URL="https://us-maven.pkg.dev/gar-prod-setup/harness-maven-public/io/harness/cloudsql-socket-factory/${CLOUDSQL_VERSION}/cloudsql-socket-factory-${CLOUDSQL_VERSION}.jar"
 
 mkdir -p /liquibase/lib
